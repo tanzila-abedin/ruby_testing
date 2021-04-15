95968431f328ef2a287cc12a7eed4d1fd0f0a5c3
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-15 23:29:17 +0200

    add test cases to exe. 9,10,11a and 11b

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -91,33 +91,40 @@ describe Drink do
     context 'when type is specified and ounces is default' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:default_drink){described_class.new('falooda')}
 
-      # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+  
+      it 'is your choice of beverage' do
+        expect(default_drink.type).to eq('falooda')
       end
 
-      # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+
+      it 'has 16 ounces' do
+        expect(default_drink.ounces).to eq(16)
       end
     end
   end
 
   describe '#full?' do
     context 'when drink has 16 ounces or more' do
+       subject(:default_drink) {described_class.new}
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+    
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(default_drink).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
+      subject(:my_drink) {described_class.new('falooda', 12)}
 
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(my_drink).to_not be_full
       end
     end
   end
--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:skylar) {described_class.new('skylar', 'great dane')}
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when dog shares the same method with multiple classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(skylar).to_not be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(skylar).to be_sleeping
   end
 end
